595b67dff613fbcfd6cdc04bcae44c4fbd51c461
soumyacodepractice-exe <soumyacodeprac@gmail.com>
2026-07-22 19:15:00 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        quickSort(nums,0,nums.length-1);
+    }
+
+    public static void quickSort(int [] nums,int left, int right){
+        if(left>= right) return;
+
+        int pivotIndex = (left+right)/2;
+        int pivot = nums[pivotIndex];
+        int i=left;
+        int j= right;
+
+        while(i<=j){
+            while(nums[i]<pivot) i++;
+            while(nums[j]>pivot) j--;
+
+            if(i<=j){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
+            }
+        }
+        quickSort(nums,left,j);
+        quickSort(nums,i,right);
+    }
+}
