59392a76c3623cdc20180d596ac9bb211dc8b829
soumyacodepractice-exe <soumyacodeprac@gmail.com>
2026-08-04 19:56:33 +0530

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+      StringBuilder sb = new StringBuilder();
+		for(char c : s.toLowerCase().toCharArray()) {
+			if(Character.isLetterOrDigit(c)) {
+				sb.append(c);
+			}
+		}
+		
+		int left=0;
+		int right = sb.length()-1;
+		while(left<right) {
+			if(sb.charAt(left)!=sb.charAt(right)) return false;
+			left++;
+			right--;
+		}
+		
+		return true;
+    }
+}
