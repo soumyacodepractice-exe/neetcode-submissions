8bf99cec2efde60b8c89c6423236056c459f8305
soumyacodepractice-exe <soumyacodeprac@gmail.com>
2026-08-27 20:47:28 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+		for(String s:operations) {
+			if(s.equals("C")) {
+				if(!stack.isEmpty()) {
+					stack.pop();
+				}
+			}else if(s.equals("D")) {
+				if(!stack.isEmpty()) {
+					stack.push(stack.peek()*2);
+				}
+			}else if(s.equals("+")) {
+				if(!stack.isEmpty()) {
+					int last = stack.pop();
+					int secondLast = stack.peek();
+					stack.push(last);
+					stack.push(last+secondLast);
+				}
+			}else {
+				stack.push(Integer.parseInt(s));
+			}
+		}
+		int total=0;
+		while(!stack.isEmpty()) {
+			total+=stack.pop();
+		}
+		return total;
+    }
+}
