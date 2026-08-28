768b9db4210384ec83f8f7cf5d615fd8138e2cb0
soumyacodepractice-exe <soumyacodeprac@gmail.com>
2026-08-28 19:54:53 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+         if(s.length() %2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+		for (char c : s.toCharArray()) {
+			if (c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char top = stack.pop();
+				if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+					return false;
+				}
+			}
+		}
+		if (!stack.isEmpty()) {
+			return false;
+		}
+        return true;
+    }
+}
